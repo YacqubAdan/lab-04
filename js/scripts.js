0a3c3bf8f9d2c62c2aceff5fbc9869bc3222be07
Yacquub Adan <yacquubadan.1@gmail.com>
2020-02-13 18:35:13 +0000

    lab 04 completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const menuToggle = document.getElementById("menu-toggle");
+const menuNav = document.getElementById("menu-nav");
+
+
+toggleMenu = () => {
+    console.log("Called toggle Menu");
+    menuNav.classList.toggle("menu-toggle")
+}
+
+menuToggle.addEventListener("click", toggleMenu);
